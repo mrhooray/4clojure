85514ac5babd15bbda0b26fe5fa3000bb18ed521
Rui Hu <code@mrhooray.com>
2013-07-20 19:48:17 -0700

    update project

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject foreclojure "0.1.0-SNAPSHOT"
-  :description "solution for 4clojure.com"
+  :description "solutions for 4clojure.com"
   :url "https://github.com/mrhooray/4clojure-solutions"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
