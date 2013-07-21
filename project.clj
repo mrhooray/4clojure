52946fe0562cbfbe95da737535af30aea44e8fdf
Rui Hu <code@mrhooray.com>
2013-07-20 23:59:36 -0700

    udpate project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject foreclojure "0.1.0-SNAPSHOT"
+(defproject foreclojure "0.1.0"
   :description "solutions for 4clojure.com"
-  :url "https://github.com/mrhooray/4clojure-solutions"
+  :url "https://github.com/mrhooray/4clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
