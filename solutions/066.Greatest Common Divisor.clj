774f87a4c80d675a6a5cffc98587516d9cb0ac68
Rui Hu <code@mrhooray.com>
2013-08-09 23:59:12 -0700

    add solution

---
+++ solutions/066.Greatest Common Divisor.clj
@@ -0,0 +1,11 @@
+; Greatest Common Divisor
+
+(def p066 ["(= (__ 2 4) 2)"
+           "(= (__ 10 5) 5)"
+           "(= (__ 5 7) 1)"
+           "(= (__ 1023 858) 33)"])
+
+(def s066 "(fn [a b]
+            (if (zero? (rem a b))
+              b
+              (recur b (rem a b))))")
